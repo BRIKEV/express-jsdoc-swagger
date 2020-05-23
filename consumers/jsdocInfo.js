a39ad1a211a509d0e0a10ac86df7bcfebbea7619
kevin Martínez <kevinccbsg@gmail.com>
2020-05-23 19:20:37 +0200

    feat: add jsdocinfo tests

--- consumers/jsdocInfo.js
+++ consumers/jsdocInfo.js
@@ -1,6 +1,7 @@
 const doctrine = require('doctrine');
 
 const jsdocInfo = (options = { unwrap: true }) => comments => {
+  if (!comments || !Array.isArray(comments)) return [];
   const parsed = comments.map(comment => doctrine.parse(comment, options));
   return parsed;
 };
