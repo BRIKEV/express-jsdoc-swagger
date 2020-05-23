0efcaff5c059b5d25fad4a3477ff5a1ddbe43785
kevin Martínez <kevinccbsg@gmail.com>
2020-05-23 14:35:03 +0200

    feat: add tests for getOnlyComments consumer

--- consumers/getOnlyComments.js
+++ consumers/getOnlyComments.js
@@ -2,9 +2,20 @@ const getComments = require('../utils/getComments');
 
 const flat = array => [].concat(...array);
 
-const getOnlyComments = files => {
-  const comments = files.map(getComments);
-  return flat(comments);
+const trimComment = comment => comment.trim();
+
+const removeSimpleComments = comment => (comment[0] === '/' && comment[1] !== '/');
+
+const processComments = comment => {
+  const trimedComments = trimComment(comment);
+  return getComments(trimedComments);
+};
+
+const getOnlyComments = (files = []) => {
+  if (!Array.isArray(files)) return [];
+  const comments = files
+    .map(processComments);
+  return flat(comments).filter(removeSimpleComments);
 };
 
 module.exports = getOnlyComments;
