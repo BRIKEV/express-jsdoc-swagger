5c1c8560ac193fa444f30b096a5691e22303d641
kevin Martínez <kevinccbsg@gmail.com>
2020-05-23 19:38:03 +0200

    feat: add tests for readfiles

--- consumers/readFiles.js
+++ consumers/readFiles.js
@@ -1,6 +1,7 @@
 const readFile = require('../utils/readFile');
 
 const readFiles = files => {
+  if (!files || !Array.isArray(files)) return Promise.resolve([]);
   const filesInfo = files.map(file => readFile(file));
   return Promise.all(filesInfo);
 };
