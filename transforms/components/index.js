03374e36a1e128d65da22ae32fdba55c0ce4e0b2
Briam Martinez <briammartinez@gmail.com>
2020-05-24 19:35:01 +0200

    Added components functionality

---
+++ transforms/components/index.js
@@ -0,0 +1,57 @@
+const { getTagInfo, getTagsInfo } = require('../utils/tags');
+
+const REQUIRED = 'required';
+
+const getPropertyName = ({ name: propertyName }) => {
+  const [name] = propertyName.split('.');
+  return name;
+};
+
+const formatProperties = properties => {
+  if (!properties || !Array.isArray(properties)) return {};
+  return properties.reduce((acum, property) => {
+    const name = getPropertyName(property);
+    const [description, format] = property.description.split(' - ');
+    return {
+      ...acum,
+      [name]: {
+        type: property.type.name,
+        description,
+        ...(format ? { format } : {}),
+      },
+    };
+  }, {});
+};
+
+const isPropertyRequired = properties => {
+  if (!properties || !Array.isArray(properties)) return [];
+  return properties.filter(({ name }) => name.includes(REQUIRED)).map(getPropertyName);
+};
+
+const parseSchema = schema => {
+  const typedef = getTagInfo(schema.tags, 'typedef');
+  const propertyValues = getTagsInfo(schema.tags, 'property');
+  return {
+    [typedef.name]: {
+      description: schema.description,
+      required: isPropertyRequired(propertyValues),
+      type: 'object',
+      properties: formatProperties(propertyValues),
+    },
+  };
+};
+
+const parseComponents = (swaggerObject = {}, components = []) => {
+  if (!components || !Array.isArray(components)) return { components: { schemas: {} } };
+  const componentSchema = components.reduce((acum, item) => ({
+    ...acum, ...parseSchema(item),
+  }), {});
+  return {
+    ...swaggerObject,
+    components: {
+      schemas: componentSchema,
+    },
+  };
+};
+
+module.exports = parseComponents;
