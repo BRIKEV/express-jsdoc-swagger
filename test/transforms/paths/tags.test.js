3eb76b14c78cf755ba885df7e2f9bc382268f812
Briam Martinez <briammartinez@gmail.com>
2020-05-29 19:39:09 +0200

    Created tags tests of paths

---
+++ test/transforms/paths/tags.test.js
@@ -0,0 +1,58 @@
+const jsdocInfo = require('../../../consumers/jsdocInfo');
+const setPaths = require('../../../transforms/paths');
+
+describe('Paths - tags', () => {
+  it('Should parse jsdoc tags params into array with tag name', () => {
+    const jsodInput = [`
+      /**
+       * GET /api/v1
+       * @tags album
+       */
+    `];
+    const expected = {
+      paths: {
+        '/api/v1': {
+          get: {
+            summary: '',
+            tags: [
+              'album',
+            ],
+            responses: {},
+            parameters: [],
+          },
+        },
+      },
+    };
+    const parsedJSDocs = jsdocInfo()(jsodInput);
+    const result = setPaths({}, parsedJSDocs);
+    expect(result).toEqual(expected);
+  });
+
+  it('Should parse jsdoc multiple tags params into array of tags', () => {
+    const jsodInput = [`
+      /**
+       * GET /api/v1
+       * @tags album
+       * @tags Years
+       */
+    `];
+    const expected = {
+      paths: {
+        '/api/v1': {
+          get: {
+            summary: '',
+            tags: [
+              'album',
+              'Years',
+            ],
+            responses: {},
+            parameters: [],
+          },
+        },
+      },
+    };
+    const parsedJSDocs = jsdocInfo()(jsodInput);
+    const result = setPaths({}, parsedJSDocs);
+    expect(result).toEqual(expected);
+  });
+});
