57ca3863e535ba729612a0731a3036a315e7e3a9
kevin Martínez <kevinccbsg@gmail.com>
2020-05-31 23:22:45 +0200

    docs: add configuration example

---
+++ examples/configuration/main.js
@@ -0,0 +1,61 @@
+const express = require('express');
+
+const expressJSDocSwagger = require('../..');
+
+// This is a full set of options
+// It is not neccesary to complete every option
+const options = {
+  info: {
+    version: '1.0.0',
+    title: 'Albums store',
+    license: {
+      name: 'MIT',
+      url: 'http://example.com',
+    },
+    description: 'API desctiption',
+    contact: {
+      name: 'contact name',
+      url: 'http://example.com',
+      email: 'test@test.com',
+    },
+    termsOfService: 'http://example.com',
+  },
+  servers: [
+    {
+      url: 'https://{username}.gigantic-server.com:{port}/{basePath}',
+      description: 'The production API server',
+      variables: {
+        username: {
+          default: 'demo',
+          description: 'this value is assigned by the service provider, in this example `gigantic-server.com`',
+        },
+        port: {
+          enum: [
+            '8443',
+            '443',
+          ],
+          default: '8443',
+        },
+        basePath: {
+          default: 'v2',
+        },
+      },
+    },
+  ],
+  file: './main.js',
+  baseDir: __dirname,
+};
+
+const app = express();
+const port = 3000;
+
+expressJSDocSwagger(app)(options);
+
+/**
+ * GET /api/v1
+ * @summary This is the summary or description of the endpoint
+ * @return {string} 200 - success response
+ */
+app.get('/api/v1', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
