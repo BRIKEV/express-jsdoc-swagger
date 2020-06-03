82729b0dc99df33184f6be94b7faa50566cc2a64
Briam Martinez <briammartinez@gmail.com>
2020-06-03 22:37:08 +0200

    test: added more security transform tests

--- test/transforms/security/index.test.js
+++ test/transforms/security/index.test.js
@@ -1,6 +1,39 @@
 const parseSecuritySchemas = require('../../../transforms/security');
 
 describe('Transform security schemas', () => {
+  it('Should return a empty object with not securitySchemes', () => {
+    const input = undefined;
+    const expected = {
+      components: {},
+    };
+    const result = parseSecuritySchemas(input);
+    expect(result).toEqual(expected);
+  });
+
+  it('Should return a empty components object with not securitySchemes when security was not set', () => {
+    const input = {
+      info: {
+        version: '1.0.0',
+        title: 'Albums store',
+        license: {
+          name: 'MIT',
+        },
+      },
+    };
+    const expected = {
+      info: {
+        version: '1.0.0',
+        title: 'Albums store',
+        license: {
+          name: 'MIT',
+        },
+      },
+      components: {},
+    };
+    const result = parseSecuritySchemas(input);
+    expect(result).toEqual(expected);
+  });
+
   it('Should return a security array and securitySchemes within components field', () => {
     const input = {
       security: {
