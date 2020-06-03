527cb49c546089b22abfb8c88f8773dadae53c57
Briam Martinez <briammartinez@gmail.com>
2020-06-03 23:14:48 +0200

    refactor: responsesGenerator method in paths transforms

--- transforms/paths/responses.js
+++ transforms/paths/responses.js
@@ -3,29 +3,31 @@ const chalk = require('chalk');
 const STATUS_CODES = require('./validStatusCodes');
 const getContent = require('./content');
 
+const mapDescription = description => description.split(' - ');
+
+const hasOldContent = (value, status) => (value[status] && value[status].content);
+
 const responsesGenerator = (returnValues = []) => {
-  const mapDescription = description => description.split(' - ');
   if (!returnValues || !Array.isArray(returnValues)) return {};
-  const response = returnValues.reduce((acc, value) => {
-    const [status, responseDescription, contentType] = mapDescription(value.description);
+  const responses = returnValues.reduce((acc, value) => {
+    const [status, description, contentType] = mapDescription(value.description);
     if (!STATUS_CODES[Number(status)]) {
       // eslint-disable-next-line
       console.warn(chalk.yellow(`Status ${status} is not valid to create a response`));
       return {};
     }
-    const hasOldContent = (acc[status] && acc[status].content);
     return {
       ...acc,
       [status]: {
-        description: responseDescription,
+        description,
         content: {
-          ...(hasOldContent ? { ...acc[status].content } : {}),
+          ...(hasOldContent(acc, status) ? { ...acc[status].content } : {}),
           ...getContent(value.type, contentType),
         },
       },
     };
   }, {});
-  return response;
+  return responses;
 };
 
 module.exports = responsesGenerator;
