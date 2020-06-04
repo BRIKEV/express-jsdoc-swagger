ded0c3959e895f7e7a8914b791ccd44c489793a9
Briam Martinez <briammartinez@gmail.com>
2020-06-05 00:22:23 +0200

    test: testing new case in tags transforms

--- test/transforms/tags/index.test.js
+++ test/transforms/tags/index.test.js
@@ -132,4 +132,30 @@ describe('parseTags method', () => {
     const result = parseTags({}, parsedJSDocs);
     expect(result).toEqual(expected);
   });
+
+  it('Should return one tag with its description', () => {
+    const jsodInput = [`
+      /**
+       * GET /api/v1/album
+       * @tags album
+       */
+    `,
+    `
+      /**
+       * GET /api/v1/years
+       * @tags album - album tag description
+       */
+    `];
+    const expected = {
+      tags: [
+        {
+          name: 'album',
+          description: 'album tag description',
+        },
+      ],
+    };
+    const parsedJSDocs = jsdocInfo()(jsodInput);
+    const result = parseTags({}, parsedJSDocs);
+    expect(result).toEqual(expected);
+  });
 });
