1a70612f9eaf16e4ce64ffee33bcd727f6934fe2
Briam Martinez <briammartinez@gmail.com>
2020-06-05 00:20:01 +0200

    test: added more paths transform tests

--- test/transforms/paths/tags.test.js
+++ test/transforms/paths/tags.test.js
@@ -57,4 +57,33 @@ describe('Paths - tags', () => {
     const result = setPaths({}, parsedJSDocs);
     expect(result).toEqual(expected);
   });
+
+  it('Should parse jsdoc multiple tags params into array of tags without description', () => {
+    const jsodInput = [`
+      /**
+       * GET /api/v1
+       * @tags album
+       * @tags Years - tag description
+       */
+    `];
+    const expected = {
+      paths: {
+        '/api/v1': {
+          get: {
+            deprecated: false,
+            summary: '',
+            tags: [
+              'album',
+              'Years',
+            ],
+            responses: {},
+            parameters: [],
+          },
+        },
+      },
+    };
+    const parsedJSDocs = jsdocInfo()(jsodInput);
+    const result = setPaths({}, parsedJSDocs);
+    expect(result).toEqual(expected);
+  });
 });
