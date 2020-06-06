1fbcab12b441afcb272cba4db63d840323c56e9e
kevin Martínez <kevinccbsg@gmail.com>
2020-06-07 01:19:26 +0200

    fix: swagger object result

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const expressJSDocSwagger = app => (
       .then(result => {
         swaggerObject = {
           ...swaggerObject,
-          result,
+          ...result,
         };
       });
 
