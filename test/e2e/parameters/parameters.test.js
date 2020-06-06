c28a02e1417b84bea1dbaad2f618575655d9da5e
kevin Martínez <kevinccbsg@gmail.com>
2020-06-07 01:42:13 +0200

    feat: add parameters e2e tests

---
+++ test/e2e/parameters/parameters.test.js
@@ -0,0 +1,126 @@
+const processSwagger = require('../../../processSwagger');
+
+const options = {
+  info: {
+    version: '1.0.0',
+    title: 'Albums store',
+    license: {
+      name: 'MIT',
+    },
+  },
+  file: './jsdoc-example.js',
+  baseDir: __dirname,
+};
+
+test('should parse components jsdoc from jsdoc-example', async () => {
+  const expected = {
+    openapi: '3.0.0',
+    info: {
+      title: 'Albums store',
+      description: 'Add your description',
+      contact: {},
+      license: { name: 'MIT', url: '' },
+      termsOfService: '',
+      version: '1.0.0',
+    },
+    servers: [],
+    security: undefined,
+    paths: {
+      '/api/v1': {
+        get: {
+          deprecated: false,
+          summary: 'This is the summary or description of the endpoint',
+          responses: {
+            200: {
+              description: 'success response',
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'string',
+                  },
+                },
+              },
+            },
+          },
+          parameters: [
+            {
+              name: 'name',
+              in: 'query',
+              description: 'name param description',
+              required: true,
+              deprecated: false,
+              allowEmptyValue: false,
+              schema: {
+                type: 'string',
+              },
+            },
+            {
+              name: 'phone',
+              in: 'param',
+              description: 'phone number',
+              required: false,
+              deprecated: false,
+              allowEmptyValue: false,
+              schema: {
+                type: 'number',
+              },
+            },
+          ],
+          tags: [],
+        },
+      },
+      '/api/v1/albums': {
+        get: {
+          deprecated: false,
+          summary: 'This is the summary or description of the endpoint',
+          responses: {
+            200: {
+              description: 'success response',
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'object',
+                  },
+                },
+              },
+            },
+          },
+          parameters: [
+            {
+              name: 'name',
+              in: 'query',
+              description: 'name param description',
+              required: true,
+              deprecated: true,
+              allowEmptyValue: false,
+              schema: {
+                type: 'array',
+                items: {
+                  type: 'string',
+                },
+              },
+            },
+            {
+              name: 'phone',
+              in: 'param',
+              description: '',
+              required: false,
+              deprecated: false,
+              allowEmptyValue: false,
+              schema: {
+                type: 'number',
+              },
+            },
+          ],
+          tags: [],
+        },
+      },
+    },
+    tags: [],
+    components: {
+      schemas: {},
+    },
+  };
+  const result = await processSwagger(options);
+  expect(result).toEqual(expected);
+});
