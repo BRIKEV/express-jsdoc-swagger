c436cd4b570df182d1676a03d0d968132ed76cd4
kevin Martínez <kevinccbsg@gmail.com>
2020-06-08 23:12:24 +0200

    feat: combine schemas for responses

---
+++ test/transforms/paths/combineSchemas.test.js
@@ -0,0 +1,154 @@
+const jsdocInfo = require('../../../consumers/jsdocInfo');
+const setPaths = require('../../../transforms/paths');
+const parseComponents = require('../../../transforms/components');
+
+test('should parse jsdoc path response with oneOf keyword', () => {
+  const jsodInput = [`
+      /**
+       * GET /api/v1
+       * @summary This is the summary or description of the endpoint
+       * @return {oneOf|Song|Album} 200 - success response - application/json
+       */
+    `];
+  const expected = {
+    paths: {
+      '/api/v1': {
+        get: {
+          deprecated: false,
+          summary: 'This is the summary or description of the endpoint',
+          parameters: [],
+          tags: [],
+          security: [],
+          responses: {
+            200: {
+              description: 'success response',
+              content: {
+                'application/json': {
+                  schema: {
+                    oneOf: [
+                      {
+                        $ref: '#/components/schemas/Song',
+                      },
+                      {
+                        $ref: '#/components/schemas/Album',
+                      },
+                    ],
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+  const parsedJSDocs = jsdocInfo()(jsodInput);
+  const result = setPaths({}, parsedJSDocs);
+  expect(result).toEqual(expected);
+});
+
+test('should not parse when type is invalid', () => {
+  global.console = { ...global.console, warn: jest.fn() };
+  const jsodInput = [`
+      /**
+       * GET /api/v1
+       * @summary This is the summary or description of the endpoint
+       * @return {invalid|Song|Album} 200 - success response - application/json
+       */
+    `];
+  const expected = {
+    paths: {
+      '/api/v1': {
+        get: {
+          deprecated: false,
+          summary: 'This is the summary or description of the endpoint',
+          parameters: [],
+          tags: [],
+          security: [],
+          responses: {
+            200: {
+              description: 'success response',
+              content: {
+                'application/json': {
+                  schema: {},
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+  const parsedJSDocs = jsdocInfo()(jsodInput);
+  const result = setPaths({}, parsedJSDocs);
+  expect(result).toEqual(expected);
+  // eslint-disable-next-line
+  expect(console.warn).toHaveBeenCalled();
+});
+
+test.skip('should not parse component with anyOf keyword', () => {
+  const jsodInput = [`
+    /**
+     * A song
+     * @typedef {object} Song
+     * @property {string} title.required
+     * @property {string} artist
+     * @property {number} year
+     */
+  `,
+  `
+    /**
+     * Album
+     * @typedef {object} Album
+     * @property {anyOf|Song|Album} firstSong
+     */
+  `];
+  const expected = {
+    components: {
+      schemas: {
+        Song: {
+          type: 'object',
+          required: [
+            'title',
+          ],
+          description: 'A song',
+          properties: {
+            title: {
+              type: 'string',
+              description: '',
+            },
+            artist: {
+              type: 'string',
+              description: '',
+            },
+            year: {
+              type: 'number',
+              description: '',
+            },
+          },
+        },
+        Album: {
+          type: 'object',
+          required: [],
+          description: 'Album',
+          properties: {
+            firstSong: {
+              description: '',
+              anyOf: [
+                {
+                  $ref: '#/components/schemas/Cat',
+                },
+                {
+                  $ref: '#/components/schemas/Dog',
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
+  };
+  const parsedJSDocs = jsdocInfo()(jsodInput);
+  const result = parseComponents({}, parsedJSDocs);
+  expect(result).toEqual(expected);
+});
--- transforms/paths/schema.js
+++ transforms/paths/schema.js
@@ -1,13 +1,36 @@
+const chalk = require('chalk');
 const refSchema = require('../utils/refSchema');
+
+const VALID_TYPES = ['oneOf', 'anyOf', 'allOf'];
+
+const validType = type => VALID_TYPES.includes(type);
+
+const combineSchema = elements => {
+  let schema = {};
+  if (!elements || elements.length === 0) return schema;
+  const schemaType = elements[0].name;
+  const [, ...types] = elements;
+  if (validType(schemaType)) {
+    schema = {
+      [schemaType]: types.map(type => refSchema(type.name)),
+    };
+  } else {
+    console.warn(chalk.yellow(`SchemaType ${schemaType} invalid, it should be one of these ${VALID_TYPES.join(', ')}`));
+  }
+  return schema;
+};
 
 const getSchema = type => {
   const nameType = type.name;
   let schema = {
     ...refSchema(nameType),
   };
+  schema = {
+    ...combineSchema(type.elements),
+  };
   const notPrimitiveType = !nameType;
-  if (notPrimitiveType) {
-    const items = type.applications || {};
+  if (notPrimitiveType && !type.elements) {
+    const items = type.applications || [];
     const parseItems = items.reduce((itemAcc, itemTypes) => ({
       ...itemAcc,
       ...refSchema(itemTypes.name),
