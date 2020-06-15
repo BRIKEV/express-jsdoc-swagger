c9107f74383150e33ca4b13f595c3709983f5f9d
kevin Martínez <kevinccbsg@gmail.com>
2020-06-15 22:46:49 +0200

    feat: add e2e test for error handling

---
+++ test/e2e/errors/jsdoc-parameter-error.js
@@ -0,0 +1,8 @@
+
+/**
+ * GET /api/v1/album
+ * @summary This is the summary or description of the endpoint
+ * @param name.query.required - name param description
+ * @param phone.param - phone number
+ * @return {string} 200 - success response
+ */
---
+++ test/e2e/errors/jsdoc-response-error.js
@@ -0,0 +1,7 @@
+
+/**
+ * GET /api/v1/album
+ * @summary This is the summary or description of the endpoint
+ * @return 200 - success response - application/json
+ * @return 400 - Bad request response
+ */
