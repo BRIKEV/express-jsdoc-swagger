82b9eca400e5c36d8309d1b5dd8eb8d7ddbe6294
kevin Martínez <kevinccbsg@gmail.com>
2020-06-15 23:05:32 +0200

    feat: invalid request body message

--- transforms/paths/parameters.js
+++ transforms/paths/parameters.js
@@ -39,15 +39,21 @@ const parameterPayload = (options, schema) => ({
   schema,
 });
 
+const wrongInOption = paramValue => {
+  if (!paramValue.includes('request.body')) {
+    // eslint-disable-next-line
+    console.warn(chalk.yellow(`If you want to add one @param as body you must provide "request.body" instead of ${paramValue}`));
+  }
+  return defaultParseParameter;
+};
+
 const parseParameter = param => {
   const [name, inOption, ...extraOptions] = param.name.split('.');
   if (!name || !inOption) {
     return defaultParseParameter;
   }
   if (inOption === BODY_PARAM) {
-    // eslint-disable-next-line
-    console.warn(chalk.yellow(`If you want to add one @param as body you must provide "request.body" instead of ${param.name}`));
-    return defaultParseParameter;
+    return wrongInOption(param.name);
   }
   const isRequired = extraOptions.includes(REQUIRED);
   const allowEmptyValue = extraOptions.includes(ALLOW_EMPTY_VALUE);
