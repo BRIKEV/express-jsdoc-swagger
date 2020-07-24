a92f79e91c2e67234614f673b8ab207002136809
Briam Martinez <briammartinez@gmail.com>
2020-07-24 18:34:25 +0200

    fix: used path instead of query to fix validation warning

--- test/e2e/parameters/jsdoc-example.js
+++ test/e2e/parameters/jsdoc-example.js
@@ -1,15 +1,15 @@
 /**
  * GET /api/v1
  * @summary This is the summary or description of the endpoint
- * @param {string} name.query.required - name param description
- * @param {number} phone.param - phone number
+ * @param {string} name.path.required - name param description
+ * @param {number} phone.path.required - phone number
  * @return {string} 200 - success response
  */
 
 /**
  * GET /api/v1/albums
  * @summary This is the summary or description of the endpoint
- * @param {array<string>} name.query.required.deprecated - name param description
- * @param {number} phone.param
+ * @param {array<string>} name.path.required - name param description
+ * @param {number} phone.path.required
  * @return {object} 200 - success response - application/json
  */
