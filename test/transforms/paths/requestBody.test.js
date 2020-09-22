19c6f22228cfdb2c945072e0d02ebb9b15a4012d
Kevin Julián Martínez Escobar <kevinccbsg@gmail.com>
2020-09-22 22:08:48 +0200

    docs: add new request body example (#87)
    * docs: add new request body example
    
    * feat: add new content type condition test
    
    * fix: typo on test file

--- test/transforms/paths/requestBody.test.js
+++ test/transforms/paths/requestBody.test.js
@@ -269,6 +269,7 @@ describe('request body tests', () => {
        * POST /api/v1
        * @param {array<Song>} request.body.required - name body description
        * @param {Song} request.body.required - name body description - application/xml
+       * @param {object} request.body.required - name body description - application/x-www-form-urlencoded
        */
     `];
     const expected = {
@@ -296,6 +297,11 @@ describe('request body tests', () => {
                 'application/xml': {
                   schema: {
                     $ref: '#/components/schemas/Song',
+                  },
+                },
+                'application/x-www-form-urlencoded': {
+                  schema: {
+                    type: 'object',
                   },
                 },
               },
