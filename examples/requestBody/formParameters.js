d803cf0227ef7b9b14e91553076e8db7545c8267
kevin Martínez <kevinccbsg@gmail.com>
2020-10-20 00:39:57 +0200

    docs: form parameters example

---
+++ examples/requestBody/formParameters.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const logger = require('../utils/logger');
+const expressJSDocSwagger = require('../..');
+
+const options = {
+  info: {
+    version: '1.0.0',
+    title: 'Albums store',
+    license: {
+      name: 'MIT',
+    },
+  },
+  filesPattern: './formParameters.js',
+  baseDir: __dirname,
+};
+
+const app = express();
+const port = 3000;
+
+expressJSDocSwagger(app)(options);
+
+/**
+ * POST /api/v1/song
+ * @param {string} id.form.required - songId - application/x-www-form-urlencoded
+ * @param {string} title.form.required - title - application/x-www-form-urlencoded
+ * @return {object} 200 - song response
+ */
+app.post('/api/v1/songs', (req, res) => res.json({}));
+
+
+app.listen(port, () => logger.info(`Example app listening at http://localhost:${port}`));
