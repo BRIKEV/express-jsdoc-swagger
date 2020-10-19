d23b286b6c84e07aadacba0bc265f65808d63f13
kevin Martínez <kevinccbsg@gmail.com>
2020-10-20 00:21:54 +0200

    feat: add more test scenarios

--- test/transforms/paths/formParameters.test.js
+++ test/transforms/paths/formParameters.test.js
@@ -48,4 +48,211 @@ describe('form requestBody tests', () => {
     const result = setPaths({}, parsedJSDocs);
     expect(result).toEqual(expected);
   });
+
+  it('should add request body using form examples with params', () => {
+    const jsodInput = [`
+      /**
+       * POST /api/v1/
+       * @param {string} name.path.required - name param description
+       * @param {string} id.form.required - id description
+       * @param {string} title.form.required - title description
+       */
+    `];
+    const expected = {
+      paths: {
+        '/api/v1/': {
+          post: {
+            deprecated: false,
+            summary: '',
+            responses: {},
+            tags: [],
+            security: [],
+            parameters: [
+              {
+                deprecated: false,
+                description: 'name param description',
+                in: 'path',
+                name: 'name',
+                required: true,
+                schema: {
+                  type: 'string',
+                },
+              },
+            ],
+            requestBody: {
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'object',
+                    properties: {
+                      id: {
+                        type: 'string',
+                        description: 'id description',
+                      },
+                      title: {
+                        type: 'string',
+                        description: 'title description',
+                      },
+                    },
+                    required: ['id', 'title'],
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+    const parsedJSDocs = jsdocInfo()(jsodInput);
+    const result = setPaths({}, parsedJSDocs);
+    expect(result).toEqual(expected);
+  });
+
+  it('should add request body for different application content type', () => {
+    const jsodInput = [`
+      /**
+       * POST /api/v1/
+       * @param {string} name.path.required - name param description
+       * @param {string} id.form.required - id description - application/x-www-form-urlencoded
+       * @param {string} title.form.required - title description
+       */
+    `];
+    const expected = {
+      paths: {
+        '/api/v1/': {
+          post: {
+            deprecated: false,
+            summary: '',
+            responses: {},
+            tags: [],
+            security: [],
+            parameters: [
+              {
+                deprecated: false,
+                description: 'name param description',
+                in: 'path',
+                name: 'name',
+                required: true,
+                schema: {
+                  type: 'string',
+                },
+              },
+            ],
+            requestBody: {
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'object',
+                    properties: {
+                      title: {
+                        type: 'string',
+                        description: 'title description',
+                      },
+                    },
+                    required: ['title'],
+                  },
+                },
+                'application/x-www-form-urlencoded': {
+                  schema: {
+                    type: 'object',
+                    properties: {
+                      id: {
+                        type: 'string',
+                        description: 'id description',
+                      },
+                    },
+                    required: ['id'],
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+    const parsedJSDocs = jsdocInfo()(jsodInput);
+    const result = setPaths({}, parsedJSDocs);
+    expect(result).toEqual(expected);
+  });
+
+  it('should add request body for different application content type with an example', () => {
+    const jsodInput = [`
+      /**
+       * POST /api/v1/
+       * @param {string} name.path.required - name param description
+       * @param {string} id.form.required - id description - application/x-www-form-urlencoded
+       * @param {string} title.form.required - title description
+       * @example request - example payload
+       * sample input string
+       */
+    `];
+    const expected = {
+      paths: {
+        '/api/v1/': {
+          post: {
+            deprecated: false,
+            summary: '',
+            responses: {},
+            tags: [],
+            security: [],
+            parameters: [
+              {
+                deprecated: false,
+                description: 'name param description',
+                in: 'path',
+                name: 'name',
+                required: true,
+                schema: {
+                  type: 'string',
+                },
+              },
+            ],
+            requestBody: {
+              content: {
+                'application/json': {
+                  schema: {
+                    type: 'object',
+                    properties: {
+                      title: {
+                        type: 'string',
+                        description: 'title description',
+                      },
+                    },
+                    required: ['title'],
+                  },
+                  examples: {
+                    example1: {
+                      summary: 'example payload',
+                      value: 'sample input string',
+                    },
+                  },
+                },
+                'application/x-www-form-urlencoded': {
+                  schema: {
+                    type: 'object',
+                    properties: {
+                      id: {
+                        type: 'string',
+                        description: 'id description',
+                      },
+                    },
+                    required: ['id'],
+                  },
+                  examples: {
+                    example1: {
+                      summary: 'example payload',
+                      value: 'sample input string',
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+    const parsedJSDocs = jsdocInfo()(jsodInput);
+    const result = setPaths({}, parsedJSDocs);
+    expect(result).toEqual(expected);
+  });
 });
