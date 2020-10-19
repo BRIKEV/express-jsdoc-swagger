7934a6b2a86533bdead09e05fbd7652de4377ff8
kevin Martínez <kevinccbsg@gmail.com>
2020-10-20 00:59:44 +0200

    docs: add header param example

---
+++ examples/parameters/headers.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const logger = require('../utils/logger');
+const expressJSDocSwagger = require('../..');
+
+const options = {
+  info: {
+    version: '1.0.0',
+    title: 'Albums store',
+    license: {
+      name: 'MIT',
+    },
+  },
+  filesPattern: './headers.js',
+  baseDir: __dirname,
+};
+
+const app = express();
+const port = 3000;
+
+expressJSDocSwagger(app)(options);
+
+/**
+ * GET /api/v1
+ * @summary This is the summary or description of the endpoint
+ * @param {string} name.header.required - name param description
+ * @return {string} 200 - success response
+ */
+app.get('/api/v1', (req, res) => res.send('Hello World!'));
+
+
+app.listen(port, () => logger.info(`Example app listening at http://localhost:${port}`));
