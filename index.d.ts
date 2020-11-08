4f6d0e9da4d36504320b993f17d8d61fdd20328d
kevinccbsg <kevinccbsg@gmail.com>
2020-11-08 22:30:31 +0100

    feat: add index.d.ts file for typescript support

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,48 @@
 // Type definitions for express-jsdoc-swagger
 // Project: https://github.com/BRIKEV/express-jsdoc-swagger
-// Definitions by: Dailos Díaz <https://github.com/ddialar>
+// Definitions by: Kevin MArtínez <https://github.com/kevinccbsg>
 // Definitions: https://github.com/BRIKEV/express-jsdoc-swagger/index.d.ts
 // TypeScript Version: 3.9.7
 
 import { EventEmitter } from 'events';
 import { Express } from 'express';
 
+interface ContactObject {
+	name: string;
+	url?: string;
+	email?: string;
+}
+
+interface LicenseObject {
+	name: string;
+	url?: string;
+	email?: string;
+}
+
+interface InfoObject {
+	title: string;
+	version: string;
+	description?: string;
+	termsOfService?: string;
+	contact?: ContactObject;
+	license?: LicenseObject;
+}
+
+interface SecurityObject {
+	name: string[];
+}
+
 interface Options {
-	info?: string;
+	info: InfoObject;
+	baseDir: string;
+	filesPattern: string;
+	security?: SecurityObject[];
 	servers?: string[];
-	security?: string;
-	baseDir?: string;
-	filesPattern?: string;
 	swaggerUIPath?: string;
 }
 
 type UserSwagger = Record<string, unknown>;
 
-type EventEmiterHandler = (options: Options, userSwagger: UserSwagger) => EventEmitter;
+type EventEmiterHandler = (options: Options, userSwagger?: UserSwagger) => EventEmitter;
 
 export default function expressJSDocSwagger(app: express.Application): EventEmiterHandler;
