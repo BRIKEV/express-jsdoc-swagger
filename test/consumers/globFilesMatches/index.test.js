f815d135b91bc43da79ec0458ae2ac16149170dc
Aleksander Stós <aleksander.stos@gmail.com>
2020-11-18 23:42:56 +0100

    test: tests for passing an array of file paths to globFilesMatches

--- test/consumers/globFilesMatches/index.test.js
+++ test/consumers/globFilesMatches/index.test.js
@@ -54,4 +54,78 @@ describe('glob Files matches method', () => {
         done();
       });
   });
+
+  describe('Array of file paths', () => {
+    it('should return error when passed empty array', done => {
+      const baseDir = __dirname;
+      const filePath = [];
+      globFilesMatches(baseDir, filePath)
+        .catch(error => {
+          expect(error.message).toEqual('if you submit an array of filesPattern it must contain at least one pattern');
+          done();
+        });
+    });
+
+    it('should return error when one of the array parameters is not a string', done => {
+      const baseDir = __dirname;
+      const filePath = ['a', 'b', 'c', 2];
+      globFilesMatches(baseDir, filePath)
+        .catch(error => {
+          expect(error.message).toEqual('all file patterns have to be strings');
+          done();
+        });
+    });
+
+    it('should return example.txt and excluded/example.txt file', done => {
+      const baseDir = __dirname;
+      const filePath = ['./**/**.txt'];
+      globFilesMatches(baseDir, filePath)
+        .then(files => {
+          const [exampleFile, excludedFile] = files;
+          expect(exampleFile.includes('example.txt')).toBe(true);
+          expect(excludedFile.includes('excluded/example.txt')).toBe(true);
+          expect(files).toHaveLength(2);
+          done();
+        });
+    });
+
+    it('should return example.txt and excluded/example.txt file if multiple paths were passed', done => {
+      const baseDir = __dirname;
+      const filePath = ['./fixtures/example.txt', './fixtures/excluded/example.txt'];
+      globFilesMatches(baseDir, filePath)
+        .then(files => {
+          const [exampleFile, excludedFile] = files;
+          expect(exampleFile.includes('example.txt')).toBe(true);
+          expect(excludedFile.includes('excluded/example.txt')).toBe(true);
+          expect(files).toHaveLength(2);
+          done();
+        });
+    });
+
+    it('should not return duplicated paths', done => {
+      const baseDir = __dirname;
+      const filePath = ['./**/**.txt', './**/**.txt', './fixtures/example.txt'];
+      globFilesMatches(baseDir, filePath)
+        .then(files => {
+          const [exampleFile, excludedFile] = files;
+          expect(exampleFile.includes('example.txt')).toBe(true);
+          expect(excludedFile.includes('excluded/example.txt')).toBe(true);
+          expect(files).toHaveLength(2);
+          done();
+        });
+    });
+
+    it('should return only example.txt file when we add exclude condition', done => {
+      const baseDir = __dirname;
+      const filePath = ['./**/**.txt'];
+      const excludedFolder = 'excluded';
+      globFilesMatches(baseDir, filePath, excludedFolder)
+        .then(files => {
+          const [exampleFile] = files;
+          expect(exampleFile.includes('example.txt')).toBe(true);
+          expect(files).toHaveLength(1);
+          done();
+        });
+    });
+  });
 });
