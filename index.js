405175d37db55e38a0f35471501f610f0b226f41
Kjell Dankert <k.dankert@wtnet.de>
2020-11-29 18:16:49 +0100

    Added options to expose swagger-ui or api-docs separately
    Stays fully backwards compatible.

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const swaggerEvents = require('./swaggerEvents');
 
 let instance = null;
 const DEFAULT_SWAGGERUI_URL = '/api-docs';
+const DEFAULT_EXPOSE_SWAGGERUI = true;
+const DEFAULT_APIDOCS_URL = '/v3/api-docs';
+const DEFAULT_EXPOSE_APIDOCS = false;
 
 const expressJSDocSwagger = app => {
   if (instance) return () => instance;
@@ -24,14 +27,22 @@ const expressJSDocSwagger = app => {
       })
       .catch(events.error);
 
-    app.use(options.swaggerUIPath || DEFAULT_SWAGGERUI_URL, (req, res, next) => {
-      swaggerObject = {
-        ...swaggerObject,
-        host: req.get('host'),
-      };
-      req.swaggerDoc = swaggerObject;
-      next();
-    }, swaggerUi.serve, swaggerUi.setup());
+    if (options.exposeSwaggerUI || DEFAULT_EXPOSE_SWAGGERUI) {
+      app.use(options.swaggerUIPath || DEFAULT_SWAGGERUI_URL, (req, res, next) => {
+        swaggerObject = {
+          ...swaggerObject,
+          host: req.get('host'),
+        };
+        req.swaggerDoc = swaggerObject;
+        next();
+      }, swaggerUi.serve, swaggerUi.setup());
+    }
+
+    if (options.exposeApiDocs || DEFAULT_EXPOSE_APIDOCS) {
+      app.get(options.apiDocsPath || DEFAULT_APIDOCS_URL, (req, res) => {
+        res.json(swaggerObject);
+      });
+    }
 
     return instance;
   };
