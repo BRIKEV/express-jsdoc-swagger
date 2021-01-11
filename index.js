af8de14a31c3220a6c6baa4c9ab32c380f9f3255
Julien <ares11@outlook.fr>
2021-01-11 14:23:40 +0100

    feat: update swagger events for only current instance doc: add example in configuration -> multipleInstance doc: add example in eventEmitter -> multipleInstance
    It was impossible to create several instances because of the variable
    "instance" to instantiate globally and not for the current swagger.
    Instance verification condition, blocked the creation of another swagger

--- index.js
+++ index.js
@@ -3,49 +3,45 @@ const merge = require('merge');
 const processSwagger = require('./processSwagger');
 const swaggerEvents = require('./swaggerEvents');
 
-let instance = null;
 const DEFAULT_SWAGGERUI_URL = '/api-docs';
 const DEFAULT_EXPOSE_SWAGGERUI = true;
 const DEFAULT_APIDOCS_URL = '/v3/api-docs';
 const DEFAULT_EXPOSE_APIDOCS = false;
 
-const expressJSDocSwagger = app => {
-  if (instance) return () => instance;
-  return (options = {}, userSwagger = {}) => {
-    const events = swaggerEvents();
-    instance = events.instance;
-    let swaggerObject = {};
+const expressJSDocSwagger = app => (options = {}, userSwagger = {}) => {
+  const events = swaggerEvents();
+  const { instance } = events;
+  let swaggerObject = {};
 
-    processSwagger(options, events.processFile)
-      .then(result => {
-        swaggerObject = {
-          ...swaggerObject,
-          ...result,
-        };
-        swaggerObject = merge.recursive(true, swaggerObject, userSwagger);
-        events.finish(swaggerObject);
-      })
-      .catch(events.error);
+  processSwagger(options, events.processFile)
+    .then(result => {
+      swaggerObject = {
+        ...swaggerObject,
+        ...result,
+      };
+      swaggerObject = merge.recursive(true, swaggerObject, userSwagger);
+      events.finish(swaggerObject);
+    })
+    .catch(events.error);
 
-    if (options.exposeSwaggerUI || DEFAULT_EXPOSE_SWAGGERUI) {
-      app.use(options.swaggerUIPath || DEFAULT_SWAGGERUI_URL, (req, res, next) => {
-        swaggerObject = {
-          ...swaggerObject,
-          host: req.get('host'),
-        };
-        req.swaggerDoc = swaggerObject;
-        next();
-      }, swaggerUi.serve, swaggerUi.setup());
-    }
+  if (options.exposeSwaggerUI || DEFAULT_EXPOSE_SWAGGERUI) {
+    app.use(options.swaggerUIPath || DEFAULT_SWAGGERUI_URL, (req, res, next) => {
+      swaggerObject = {
+        ...swaggerObject,
+        host: req.get('host'),
+      };
+      req.swaggerDoc = swaggerObject;
+      next();
+    }, swaggerUi.serve, swaggerUi.setup());
+  }
 
-    if (options.exposeApiDocs || DEFAULT_EXPOSE_APIDOCS) {
-      app.get(options.apiDocsPath || DEFAULT_APIDOCS_URL, (req, res) => {
-        res.json(swaggerObject);
-      });
-    }
+  if (options.exposeApiDocs || DEFAULT_EXPOSE_APIDOCS) {
+    app.get(options.apiDocsPath || DEFAULT_APIDOCS_URL, (req, res) => {
+      res.json(swaggerObject);
+    });
+  }
 
-    return instance;
-  };
+  return instance;
 };
 
 module.exports = expressJSDocSwagger;
