3059ed788277e24898bcb12fa7183e1e1b7fa8f8
Mohamed Meabed <mo.meabed@gmail.com>
2021-01-31 14:45:17 +0400

    chore: support new operator

--- test/transforms/paths/combineSchemas.test.js
+++ test/transforms/paths/combineSchemas.test.js
@@ -47,6 +47,152 @@ test('should parse jsdoc path response with oneOf keyword', () => {
   expect(result).toEqual(expected);
 });
 
+test('should not parse when type is invalid', () => {
+  global.console = { ...global.console, warn: jest.fn() };
+  const jsodInput = [`
+      /**
+       * GET /api/v1
+       * @summary This is the summary or description of the endpoint
+       * @return {invalid|Song|Album} 200 - success response - application/json
+       */
+    `];
+  const expected = {
+    paths: {
+      '/api/v1': {
+        get: {
+          deprecated: false,
+          summary: 'This is the summary or description of the endpoint',
+          parameters: [],
+          tags: [],
+          security: [],
+          responses: {
+            200: {
+              description: 'success response',
+              content: {
+                'application/json': {
+                  schema: {},
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+  const parsedJSDocs = jsdocInfo()(jsodInput);
+  const result = setPaths({}, parsedJSDocs);
+  expect(result).toEqual(expected);
+  // eslint-disable-next-line
+  expect(console.warn).toHaveBeenCalled();
+});
+
+test('should not parse component with anyOf keyword', () => {
+  const jsodInput = [`
+    /**
+     * A song
+     * @typedef {object} Song
+     * @property {string} title.required
+     * @property {string} artist
+     * @property {number} year
+     */
+  `,
+  `
+    /**
+     * Album
+     * @typedef {object} Album
+     * @property {anyOf|Song|Album} firstSong
+     */
+  `];
+  const expected = {
+    components: {
+      schemas: {
+        Song: {
+          type: 'object',
+          required: [
+            'title',
+          ],
+          description: 'A song',
+          properties: {
+            title: {
+              type: 'string',
+              description: '',
+            },
+            artist: {
+              type: 'string',
+              description: '',
+            },
+            year: {
+              type: 'number',
+              description: '',
+            },
+          },
+        },
+        Album: {
+          type: 'object',
+          description: 'Album',
+          properties: {
+            firstSong: {
+              description: '',
+              anyOf: [
+                {
+                  $ref: '#/components/schemas/Song',
+                },
+                {
+                  $ref: '#/components/schemas/Album',
+                },
+              ],
+            },
+          },
+        },
+      },
+    },
+  };
+  const parsedJSDocs = jsdocInfo()(jsodInput);
+  const result = parseComponents({}, parsedJSDocs);
+  expect(result).toEqual(expected);
+});
+
+test('should parse jsdoc path reference params with allOf keyword', () => {
+  const jsodInput = [`
+    /**
+     * GET /api/v1
+     * @param {allOf|Song|Album} name.query.required - name param description
+     */
+  `];
+  const expected = {
+    paths: {
+      '/api/v1': {
+        get: {
+          deprecated: false,
+          summary: '',
+          responses: {},
+          tags: [],
+          security: [],
+          parameters: [{
+            deprecated: false,
+            description: 'name param description',
+            in: 'query',
+            name: 'name',
+            required: true,
+            schema: {
+              allOf: [
+                {
+                  $ref: '#/components/schemas/Song',
+                },
+                {
+                  $ref: '#/components/schemas/Album',
+                },
+              ],
+            },
+          }],
+        },
+      },
+    },
+  };
+  const parsedJSDocs = jsdocInfo()(jsodInput);
+  const result = setPaths({}, parsedJSDocs);
+  expect(result).toEqual(expected);
+});
 test('should not parse component with anyOf array keyword', () => {
   const jsodInput = [`
     /**
@@ -122,47 +268,5 @@ test('should not parse component with anyOf array keyword', () => {
   };
   const parsedJSDocs = jsdocInfo()(jsodInput);
   const result = parseComponents({}, parsedJSDocs);
-  expect(result).toEqual(expected);
-});
-
-test('should parse jsdoc path reference params with allOf keyword', () => {
-  const jsodInput = [`
-    /**
-     * GET /api/v1
-     * @param {allOf|Song|Album} name.query.required - name param description
-     */
-  `];
-  const expected = {
-    paths: {
-      '/api/v1': {
-        get: {
-          deprecated: false,
-          summary: '',
-          responses: {},
-          tags: [],
-          security: [],
-          parameters: [{
-            deprecated: false,
-            description: 'name param description',
-            in: 'query',
-            name: 'name',
-            required: true,
-            schema: {
-              allOf: [
-                {
-                  $ref: '#/components/schemas/Song',
-                },
-                {
-                  $ref: '#/components/schemas/Album',
-                },
-              ],
-            },
-          }],
-        },
-      },
-    },
-  };
-  const parsedJSDocs = jsdocInfo()(jsodInput);
-  const result = setPaths({}, parsedJSDocs);
   expect(result).toEqual(expected);
 });
--- test/transforms/paths/responses.test.js
+++ test/transforms/paths/responses.test.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk');
 const jsdocInfo = require('../../../consumers/jsdocInfo');
 const setPaths = require('../../../transforms/paths');
 
@@ -243,6 +244,300 @@ describe('response tests', () => {
   });
 
   it('should parse jsdoc path spec with more than one response and multiple content types', () => {
+    const jsodInput = [`
+      /**
+       * GET /api/v1
+       * @summary This is the summary or description of the endpoint
+       * @return {object} 200 - success response - application/json
+       * @return {object} 400 - Bad request response
+       * @return {string} 400 - Bad request response - application/xml
+       */
+    `];
+    const expected = {
+      paths: {
+        '/api/v1': {
+          get: {
+            deprecated: false,
+            summary: 'This is the summary or description of the endpoint',
+            parameters: [],
+            tags: [],
+            security: [],
+            responses: {
+              200: {
+                description: 'success response',
+                content: {
+                  'application/json': {
+                    schema: {
+                      type: 'object',
+                    },
+                  },
+                },
+              },
+              400: {
+                description: 'Bad request response',
+                content: {
+                  'application/json': {
+                    schema: {
+                      type: 'object',
+                    },
+                  },
+                  'application/xml': {
+                    schema: {
+                      type: 'string',
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+    const parsedJSDocs = jsdocInfo()(jsodInput);
+    const result = setPaths({}, parsedJSDocs);
+    expect(result).toEqual(expected);
+  });
+
+  it('should parse jsdoc path response with examples', () => {
+    const jsdocInput = [`
+      /**
+       * GET /api/v1
+       * @summary This is the summary or description of the endpoint
+       * @return {Song} 200 - success response - application/json
+       * @return {object} 403 - forbidden response - application/json
+       * @example response - 200 - example success response
+       * {
+       *   "title": "untitled song",
+       *   "artist": "anonymous"
+       * }
+       * @example response - 403 - example error response
+       * {
+       *   "error": "failed to retrieve results"
+       * }
+       */
+    `];
+    const expected = {
+      paths: {
+        '/api/v1': {
+          get: {
+            deprecated: false,
+            summary: 'This is the summary or description of the endpoint',
+            parameters: [],
+            tags: [],
+            security: [],
+            responses: {
+              200: {
+                description: 'success response',
+                content: {
+                  'application/json': {
+                    schema: {
+                      $ref: '#/components/schemas/Song',
+                    },
+                    examples: {
+                      example1: {
+                        summary: 'example success response',
+                        value: {
+                          title: 'untitled song',
+                          artist: 'anonymous',
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+              403: {
+                description: 'forbidden response',
+                content: {
+                  'application/json': {
+                    schema: {
+                      type: 'object',
+                    },
+                    examples: {
+                      example2: {
+                        summary: 'example error response',
+                        value: {
+                          error: 'failed to retrieve results',
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+    const parsedJSDocs = jsdocInfo()(jsdocInput);
+    const result = setPaths({}, parsedJSDocs);
+    expect(result).toEqual(expected);
+  });
+
+  it('should not parse jsdoc path response with examples when an application/json example is malformed', () => {
+    const jsdocInput = [`
+      /**
+       * GET /api/v1
+       * @summary This is the summary or description of the endpoint
+       * @return {Song} 200 - success response - application/json
+       * @return {object} 403 - forbidden response - application/json
+       * @example response - 200 - example success response
+       * {
+       *   "title": "untitled song",
+       *   "artist": "anonymous"
+       * }
+       * @example response - 403 - example error response
+       * {
+       *   "error": "failed to retrieve results",
+       * }
+       */
+    `];
+    global.console = { ...global.console, warn: jest.fn() };
+    const parsedJSDocs = jsdocInfo()(jsdocInput);
+    setPaths({}, parsedJSDocs);
+    // eslint-disable-next-line
+    expect(console.warn).toHaveBeenCalledTimes(1);
+    // eslint-disable-next-line
+    expect(console.warn).toHaveBeenNthCalledWith(
+      1,
+      chalk.yellow('[express-jsdoc-swagger] response example for status 403 with content-type application/json malformed'),
+    );
+  });
+
+  it('should parse undefined if example has no valid types (request or response)', () => {
+    const jsdocInput = [`
+      /**
+       * GET /api/v1
+       * @summary This is the summary or description of the endpoint
+       * @return {Song} 200 - success response - application/json
+       * @return {object} 403 - forbidden response - application/json
+       * @example response - 200 - example success response
+       * {
+       *   "title": "untitled song",
+       *   "artist": "anonymous"
+       * }
+       * @example res - 403 - example error response
+       * {
+       *   "error": "failed to retrieve results"
+       * }
+       */
+    `];
+    const expected = {
+      paths: {
+        '/api/v1': {
+          get: {
+            deprecated: false,
+            summary: 'This is the summary or description of the endpoint',
+            parameters: [],
+            tags: [],
+            security: [],
+            responses: {
+              200: {
+                description: 'success response',
+                content: {
+                  'application/json': {
+                    schema: {
+                      $ref: '#/components/schemas/Song',
+                    },
+                    examples: {
+                      example1: {
+                        summary: 'example success response',
+                        value: {
+                          title: 'untitled song',
+                          artist: 'anonymous',
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+              403: {
+                description: 'forbidden response',
+                content: {
+                  'application/json': {
+                    schema: {
+                      type: 'object',
+                    },
+                    examples: undefined,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+    const parsedJSDocs = jsdocInfo()(jsdocInput);
+    const result = setPaths({}, parsedJSDocs);
+    expect(result).toEqual(expected);
+  });
+
+  it('should not parse an example if has no valid status', () => {
+    const jsdocInput = [`
+      /**
+       * GET /api/v1
+       * @summary This is the summary or description of the endpoint
+       * @return {Song} 200 - success response - application/json
+       * @return {object} 403 - forbidden response - application/json
+       * @example response - 200 - example success response
+       * {
+       *   "title": "untitled song",
+       *   "artist": "anonymous"
+       * }
+       * @example response - 333 - example error response
+       * {
+       *   "error": "failed to retrieve results"
+       * }
+       */
+    `];
+    const expected = {
+      paths: {
+        '/api/v1': {
+          get: {
+            deprecated: false,
+            summary: 'This is the summary or description of the endpoint',
+            parameters: [],
+            tags: [],
+            security: [],
+            responses: {
+              200: {
+                description: 'success response',
+                content: {
+                  'application/json': {
+                    schema: {
+                      $ref: '#/components/schemas/Song',
+                    },
+                    examples: {
+                      example1: {
+                        summary: 'example success response',
+                        value: {
+                          title: 'untitled song',
+                          artist: 'anonymous',
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+              403: {
+                description: 'forbidden response',
+                content: {
+                  'application/json': {
+                    schema: {
+                      type: 'object',
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    };
+    const parsedJSDocs = jsdocInfo()(jsdocInput);
+    const result = setPaths({}, parsedJSDocs);
+    expect(result).toEqual(expected);
+  });
+  it('should parse jsdoc path spec with more than multiple response and multiple content types', () => {
     const jsodInput = [`
       /**
        * GET /api/v1
