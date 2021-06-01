8095202c21ef69cb068533fa87efa6828b1e77fe
kevinccbsg <kevinccbsg@gmail.com>
2021-06-01 22:59:27 +0200

    feat: add multiple security headers

---
+++ transforms/paths/security.js
@@ -0,0 +1,29 @@
+const { flatArray } = require('../utils/arrays');
+
+const AND_SEPARATOR = ' & ';
+const OR_SEPARATOR = ' | ';
+
+const formatOrValues = ({ description }) => {
+  const securityNames = description.split(OR_SEPARATOR);
+  return securityNames.map(names => ({
+    description: names,
+  }));
+};
+
+const formatSecurity = (securityValues = []) => (
+  flatArray(securityValues
+    .map(formatOrValues))
+    .map(({ description }) => {
+      const securityNames = description.split(AND_SEPARATOR);
+      return {
+        ...securityNames.reduce((acum, names) => (
+          {
+            ...acum,
+            [names]: [],
+          }
+        ), {}),
+      };
+    })
+);
+
+module.exports = formatSecurity;
